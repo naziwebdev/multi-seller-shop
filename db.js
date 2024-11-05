d9ac805bdc6ea91d85832a111ab17218de001392
nazi <nazipaeez777@gmail.com>
2024-11-05 20:06:40 +0330

    Init db

---
+++ db.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+const configs = require("./configs");
+
+const db = new Sequelize({
+  host: configs.db.host,
+  port: configs.db.port,
+  username: configs.db.user,
+  password: configs.db.password,
+  database: configs.db.name,
+  dialect: configs.db.dialect,
+  logging: false,
+});
+
+module.exports = { db };
