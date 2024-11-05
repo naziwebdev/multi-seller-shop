5e34cb235224bb9f893108cc6c97bad10bc6792d
nazi <nazipaeez777@gmail.com>
2024-11-05 22:10:39 +0330

    Create cors header config

---
+++ middlewares/headers.js
@@ -0,0 +1,10 @@
+exports.setHeaders = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+};
