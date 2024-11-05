4bec246e9e8cd5325d32fd7b6c8b0849fa68f7f6
nazi <nazipaeez777@gmail.com>
2024-11-05 20:46:43 +0330

    Create address migration

--- migrations/20241105161724-create-address.js
+++ migrations/20241105161724-create-address.js
@@ -1,28 +1,47 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Addresses', {
+    await queryInterface.createTable("dddresses", {
       id: {
+        type: Sequelize.INTEGER.UNSIGNED,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
       },
-      city: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
+      name: {
+        type: Sequelize.STRING,
         allowNull: false,
-        type: Sequelize.DATE
       },
-      updatedAt: {
+      postalCode: {
+        type: Sequelize.STRING,
         allowNull: false,
-        type: Sequelize.DATE
-      }
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.JSONB, // store a object
+        allowNull: false,
+        validate: {
+          isValidLocation(value) {
+            if (!value.lat || typeof value.lat !== "number") {
+              throw new Error(
+                "Latitude (lat) is required and must be a number"
+              );
+            }
+            if (!value.lng || typeof value.lng !== "number") {
+              throw new Error(
+                "Longitude (lng) is required and must be a number"
+              );
+            }
+          },
+        },
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Addresses');
-  }
+    await queryInterface.dropTable("dddresses");
+  },
 };
