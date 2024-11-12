d61ba640d1b25db56416c88cbd461b1f6f846128
nazi <nazipaeez777@gmail.com>
2024-11-12 22:18:46 +0330

    Develope update filter category api

--- controllers/v1/filtersCategory.js
+++ controllers/v1/filtersCategory.js
@@ -1,3 +1,4 @@
+const { ExclusionConstraintError } = require("sequelize");
 const { FiltersCategory, Category, SubCategory } = require("../../db");
 const {
   createFilterCategoryValidator,
@@ -69,6 +70,70 @@ exports.create = async (req, res, next) => {
 };
 exports.edit = async (req, res, next) => {
   try {
+    const { filterId } = req.params;
+
+    const {
+      name,
+      slug,
+      description,
+      type,
+      options,
+      min,
+      max,
+      category_id,
+      subCategory_id,
+    } = req.body;
+
+    if (
+      filterId === undefined ||
+      filterId === null ||
+      filterId === "" ||
+      isNaN(filterId)
+    ) {
+      return res.status(422).json({ message: "filterId is not valid" });
+    }
+
+    await editFilterCategoryValidator.validate(req.body);
+
+    const filter = await FiltersCategory.findOne({ where: { id: filterId } });
+    if (!filter) {
+      return res.status(404).json({ message: "not fount filter" });
+    }
+
+    if (category_id) {
+      existCategoryParent = await Category.findOne({
+        where: { id: category_id },
+      });
+    }
+
+    if (subCategory_id) {
+      existSubCategoryParent = await SubCategory.findOne({
+        where: { id: subCategory_id },
+      });
+    }
+
+    if (!existCategoryParent && !existSubCategoryParent) {
+      return res.status(400).json({
+        message: "at least one of category_id or subCategory_id is required",
+      });
+    }
+
+    await FiltersCategory.update(
+      {
+        name,
+        slug,
+        description,
+        type,
+        options,
+        min,
+        max,
+        category_id,
+        subCategory_id,
+      },
+      { where: { id: filterId } }
+    );
+
+    return res.status(200).json({ message: "filter updated successfully" });
   } catch (error) {
     next(error);
   }
@@ -76,12 +141,14 @@ exports.edit = async (req, res, next) => {
 exports.remove = async (req, res, next) => {
   try {
   } catch (error) {
+  
     next(error);
   }
 };
 
 exports.getOneCategoryFilters = async (req, res, next) => {
   try {
+
   } catch (error) {
     next(error);
   }
