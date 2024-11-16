c321bd2698cec0323b8c28775620b124b9ab3093
nazi <nazipaeez777@gmail.com>
2024-11-17 00:24:29 +0330

    Develope remove seller-request controller

--- controllers/v1/sellerRequest.js
+++ controllers/v1/sellerRequest.js
@@ -78,6 +78,7 @@ exports.getAllSellerRequests = async (req, res, next) => {
     next(error);
   }
 };
+
 exports.edit = async (req, res, next) => {
   try {
   } catch (error) {
@@ -86,6 +87,32 @@ exports.edit = async (req, res, next) => {
 };
 exports.remove = async (req, res, next) => {
   try {
+    const { reqId } = req.params;
+
+    const seller = req.user;
+
+    const existSeller = await Seller.findOne({ where: { id: seller.id } });
+    if (!existSeller) {
+      return res.status(404).json({ message: "not found seller" });
+    }
+
+    if (reqId === undefined || reqId === null || reqId === "" || isNaN(reqId)) {
+      return res.status(422).json({ message: "reqId is not valid" });
+    }
+
+    const existRequest = await SellerRequest.findOne({
+      where: { id: reqId, status: "pendding" },
+    });
+    if (!existRequest) {
+      return res.status(404).json({ message: "not found request" });
+    }
+
+    if (existRequest.seller_id.toString() !== seller.id.toString()) {
+      return res.status(401).json({ message: "forbidden route" });
+    }
+
+    await SellerRequest.destroy({ where: { id: reqId } });
+    return res.status(200).json({ message: "request deleted successfully" });
   } catch (error) {
     next(error);
   }
