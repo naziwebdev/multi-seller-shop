397339be41117d7b3d844f40bec314102f21de1f
nazi <nazipaeez777@gmail.com>
2024-11-16 20:45:30 +0330

    develope create product controller

--- controllers/v1/product.js
+++ controllers/v1/product.js
@@ -1,7 +1,67 @@
-const { Product, SellersProduct } = require("../../db");
+const { v4: uuidv4 } = require("uuid");
+const { Product, SellersProduct, SubCategory } = require("../../db");
+const {
+  createProductValidator,
+  editProductValidator,
+} = require("../../validators/product");
 
 exports.create = async (req, res, next) => {
   try {
+    let {
+      name,
+      slug,
+      description,
+      filtersValue,
+      customFilters,
+      subCategory_id,
+    } = req.body;
+
+    // get json from form-data => need to parse
+    //if get from json normal format dont nedd to parse
+    filtersValue = JSON.parse(filtersValue);
+    customFilters = JSON.parse(customFilters);
+
+    await createProductValidator.validate(
+      { name, slug, description, filtersValue, customFilters, subCategory_id },
+      { abortEarly: false }
+    );
+
+    let images = [];
+
+    if (req.files) {
+      const imagesFiles = req.files;
+      for (let i = 0; i < imagesFiles.length; i++) {
+        const file = req.files[i];
+        const pathFile = `images/products/${file.filename}`;
+        images.push(pathFile);
+      }
+    }
+
+    const existSubCategory = await SubCategory.findOne({
+      where: { id: subCategory_id },
+    });
+    if (!existSubCategory) {
+      return res.status(404).json({ message: "not found sub_category_id" });
+    }
+
+    const shortIdentifier = uuidv4()
+      .replace(/[^a-z]/g, "")
+      .substring(0, 6).toUpperCase()
+
+     
+
+    await Product.create({
+      name,
+      slug,
+      description,
+      filtersValue,
+      customFilters,
+      subCategory_id,
+      images,
+      shortIdentifier,
+    });
+
+    return res.status(201).json({ message: "product created successfully" });
   } catch (error) {
     next(error);
   }
