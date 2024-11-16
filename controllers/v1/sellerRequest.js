79b0ab334f1789bbfc5297a700129cc455c4b84a
nazi <nazipaeez777@gmail.com>
2024-11-17 00:44:34 +0330

    Develope reject/accept seller controller

--- controllers/v1/sellerRequest.js
+++ controllers/v1/sellerRequest.js
@@ -1,4 +1,4 @@
-const { SellerRequest, Product, Seller } = require("../../db");
+const { SellerRequest, Product, Seller, SellersProduct } = require("../../db");
 const {
   createSellerRequestValidator,
   updateSellerRequestValidator,
@@ -81,6 +81,61 @@ exports.getAllSellerRequests = async (req, res, next) => {
 
 exports.edit = async (req, res, next) => {
   try {
+    const { reqId } = req.params;
+    const { status, adminComment } = req.body;
+    await updateSellerRequestValidator.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (reqId === undefined || reqId === null || reqId === "" || isNaN(reqId)) {
+      return res.status(422).json({ message: "reqId is not valid" });
+    }
+
+    const requestSeller = await SellerRequest.findOne({
+      where: { id: reqId, status: "pendding" },
+    });
+    if (!requestSeller) {
+      return res.status(404).json({ message: "not found request" });
+    }
+
+    await SellerRequest.update(
+      { status, adminComment },
+      { where: { id: reqId } }
+    );
+
+    if (status === "accepted") {
+      const product = await Product.findOne({
+        where: { id: requestSeller.product_id },
+      });
+      if (!product) {
+        return res.status(404).json({ message: "not found product" });
+      }
+
+      const existProductSeller = await SellersProduct.findOne({
+        where: {
+          seller_id: requestSeller.seller_id,
+          product_id: requestSeller.product_id,
+        },
+      });
+      if (existProductSeller) {
+        return res
+          .status(400)
+          .json({ message: "seller exist already for this product" });
+      }
+
+      await SellersProduct.create({
+        price: requestSeller.price,
+        stock: requestSeller.stock,
+        seller_id: requestSeller.seller_id,
+        product_id: requestSeller.product_id,
+      });
+
+      return res
+        .status(201)
+        .json({ message: "seller req accepted successfully" });
+    } else if (status === "rejected") {
+      return res.status(200).json({ message: "seller req rejected" });
+    }
   } catch (error) {
     next(error);
   }
